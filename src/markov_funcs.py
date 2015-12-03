a5064eca0b0257d58b580800ebd88bc02e19c456
John Gilling <suddensleep@gmail.com>
2015-12-03 10:33:03 -0500

    added markov models and pickled files to source code

---
+++ src/markov_funcs.py
@@ -0,0 +1,282 @@
+###################################################
+###   markov_funcs.py -- code by John Gilling   ###
+### Helper functions for creating Markov Chains ###
+### from sequences of melodies and sequences of ###
+### rhythms.  Can be used on any directory with ###
+### MIDI files inside of it. Enjoy!             ###
+###################################################
+
+import sys
+import midi_funcs as midf
+import markov_sequences as marks
+import itertools
+import pickle
+from math import modf
+from bisect import bisect
+
+### Global list of increasingly fine segmentations of a beat
+level_lists = [[0.0, 1/4.0, 1/3.0, 1/2.0, 2/3.0, 3/4.0, 1.0], 
+               [0.0, 1/8.0, 1/6.0, 1/4.0, 1/3.0, 3/8.0, 1/2.0, 
+                5/8.0, 2/3.0, 3/4.0, 5/6.0, 7/8.0, 1.0],
+               [0.0, 1/16.0, 1/12.0, 1/9.0, 1/8.0, 1/6.0, 3/16.0, 
+                2/9.0, 1/4.0, 5/16.0, 1/3.0, 3/8.0, 5/12.0, 
+                7/16.0, 4/9.0, 1/2.0, 5/9.0, 9/16.0, 7/12.0, 
+                5/8.0, 2/3.0, 11/16.0, 3/4.0, 7/9.0, 13/16.0, 
+                5/6.0, 7/8.0, 8/9.0, 11/12.0, 15/16.0, 1.0]]
+
+
+def myround(tick, level = 1):
+    """
+    Rounds a tick value to the nearest fractional beat interval
+    at the given level. The levels are defined as:
+
+    Level 1: Thirds and Fourths
+    Level 2: Level 1 fractions, Sixths, and Eighths
+    Level 3: Level 2 fractions, Ninths, Twelfths, and Sixteenths
+
+    Inputs: 
+    tick is a floating point time value in units of beats
+    level is one of {1, 2, 3} as defined above
+
+    Outputs: quantized tick value
+    """
+
+    if int(tick) == tick:
+        return tick
+
+    level_list = level_lists[level - 1]
+    
+    frac_tick, whole_tick = modf(tick)
+
+    insert = bisect(level_list, frac_tick)
+
+    dist1 = abs(frac_tick - level_list[insert - 1])
+    dist2 = abs(frac_tick - level_list[insert])
+
+    if dist1 < dist2:
+        return round(whole_tick + level_list[insert - 1], 4)
+    
+    return round(whole_tick + level_list[insert], 4)
+
+
+def quantize(rhythms, level = 1):
+    """
+    Takes a list of lists of rhythms, and quantizes them (i.e.
+    rounds them to the nearest relevant fraction at the given 
+    defined level -- see definitions in docstring of myround above).
+
+    Inputs:
+    rhythms is a list of lists of integers representing rhythmic
+    timestamps
+
+    level is one of {1, 2, 3} as defined in the myround docstring
+
+    Outputs:
+    rounded copy of rhythms
+    """
+    
+    quantized_rhythms = []
+    for rhythm in rhythms:
+        inner = []
+        for tick in rhythm:
+            new_tick = myround(tick, level)
+            if len(inner) > 0 and new_tick == inner[-1]:
+                continue
+            inner.append(new_tick)
+        quantized_rhythms.append(inner)
+    
+    assert len(quantized_rhythms) == len(rhythms)
+
+    return quantized_rhythms
+
+
+def iterate_melody(mel, mark):
+    """
+    Adds one new melodic observation to the Markov Chain 
+    training process.
+
+    Inputs: 
+    mel is a list of lists representing a melodic sequence
+    mark is a markov_sequences.Markov object
+    
+    Outputs: None
+    """
+    
+    before, after = mark.before, mark.after
+    full_length = before + after + 1
+    for i in range(len(mel) - full_length):
+        for seq in itertools.product(*mel[i:i + full_length]):
+            before_seq = seq[:before]
+            after_seq = seq[-after:]
+            val = seq[before]
+            if full_length > 3:
+                before_seq, after_seq, val = recenter(before_seq, 
+                                                      after_seq, 
+                                                      val)
+            mode = mark.mode
+            if mode == 0:
+                mark.add_data(before_seq, val)
+            elif mode == 1:
+                mark.add_data(after_seq, val)
+            else:
+                mark.add_data((before_seq, after_seq), val)
+
+
+def iterate_rhythm(rhy, mark):
+    """
+    Adds one new rhythmic observation to the Markov Chain 
+    training process.
+
+    Inputs: 
+    rhy is a list of lists representing a rhythmic sequence
+    mark is a markov_sequences.Markov object
+    
+    Outputs: None
+    """
+
+    before, after = mark.before, mark.after
+    full_length = before + after + 1
+
+    for i in range(len(rhy) - full_length):
+        seq = rhy[i:i + full_length]
+        before_seq = seq[:before]
+        after_seq = seq[-after:]
+        val = seq[before]
+        before_seq, after_seq, val = recenter(before_seq, after_seq, val)
+        mode = mark.mode
+        if mode == 0:
+            mark.add_data(before_seq, val)
+        elif mode == 1:
+            mark.add_data(after_seq, val)
+        else:
+            mark.add_data((before_seq, after_seq), val)
+
+
+def recenter(before, after, val):
+    """
+    Recenters sequence(s) so that the first note value 
+    encountered is reset to 0 and every other note value 
+    is relative to the first.
+
+    Example:
+    before = [45, 47], after = [44, 45], val = 40
+    RETURNS ([0, 2], [-1, 0], -5)
+
+    before = [], after = [100, 100, 99], val = 80
+    RETURNS ([], [0, 0, 1], -20)
+
+    Inputs:
+    before and after are lists of note values
+    val is an integral pitch value for a note
+
+    Outputs:
+    Tuple of length 3 of the scaled before, after and 
+    val note values.
+    """
+
+    if len(before) == 0:
+        to_subtract = after[0]
+    else:
+        to_subtract = before[0]
+        
+    return (tuple(map(lambda x: round(x - to_subtract, 4), before)), 
+            tuple(map(lambda x: round(x - to_subtract, 4), after)),
+            round(val - to_subtract, 4))
+
+
+def make_melody_chains(mels, max_order = 2):
+    """
+    Create pickled Markov Chain melody models with a 
+    limit on the order.
+
+    Inputs: 
+    mels is the list of melody lists
+    max_order is the largest 'previous state' to allow
+
+    Outputs:
+    None (pickles files)
+    """
+
+    for before in range(max_order + 1):
+        for after in range(max_order + 1):
+            if before + after > max_order:
+                continue
+            if before == 0:
+                if after == 0:
+                    continue
+                mode = 1
+            elif after == 0:
+                mode = 0
+            else:
+                mode = 2
+            mark = marks.Markov(before, after, mode)
+            for k in range(len(mels)):
+                iterate_melody(mels[k], mark)
+            mark.normalize()
+            with open("../pickles/markov_melody_" + str(before) + str(after) 
+                      + str(mode) + ".pkl", "w") as f:
+                pickle.dump(mark, f)
+
+
+def make_rhythm_chains(rhys, max_order = 2):
+    """
+    Create pickled Markov Chain rhythm models with a 
+    limit on the order.
+
+    Inputs: 
+    rhys is the list of rhythm lists
+    max_order is the largest 'previous state' to allow
+
+    Outputs:
+    None (pickles files)
+    """
+    
+    for before in range(max_order + 1):
+        for after in range(max_order + 1):
+            if before + after > max_order:
+                continue
+            if before == 0:
+                if after == 0:
+                    continue
+                mode = 1
+            elif after == 0:
+                mode = 0
+            else:
+                mode = 2
+            mark = marks.Markov(before, after, mode)
+            for k in range(len(rhys)):
+                iterate_rhythm(rhys[k], mark)
+            mark.normalize()
+            with open("../pickles/markov_rhythm_" + str(before) + str(after)
+                      + str(mode) + ".pkl", "w") as f:
+                pickle.dump(mark, f)
+
+
+def make_all_chains(midi_path = '../midi/'):
+    """
+    Calls up midi files from the given path and converts the
+    streams into Markov Chains.
+
+    Inputs: midi_path is a string path name
+
+    Outputs: None
+    """
+
+    midi_list = midf.get_midi_list(midi_path)
+    melodies, rhythms = midf.extract_all_sequences(midi_list)
+    
+    make_melody_chains(melodies)
+    
+    rhythms = quantize(rhythms)
+    make_rhythm_chains(rhythms)
+
+
+def main(*args):
+    try:
+        path = args[1]
+        make_all_chains(path)
+    except IndexError:
+        make_all_chains()
+
+if __name__ == '__main__':
+    main(*sys.argv)
---
+++ src/markov_sequences.py
@@ -0,0 +1,96 @@
+########################################################
+###    markov_sequences.py -- code by John Gilling   ###
+### A container for holding Markov chains of various ###
+### orders, where the events in the given 'current   ###
+### state' may occur temporally before and/or after  ###
+### the note to be examined as the 'next state'.     ###
+########################################################
+
+
+class Markov(object):
+    """
+    A container for holding Markov chains of various orders.
+    """
+    
+    def __init__(self, before = 0, after = 0, mode = 0):
+        """
+        Initialize a Markov object with `before` notes before 
+        the space to be filled, `after` notes after the space 
+        to be filled, and with `mode` equal to 0, 1 or 2 
+        according to whether the 'current state' contains 
+        notes only before, only after, or both before and 
+        after the note to be filled in the 'next state'.
+        
+        Inputs: 
+        before - int - number of notes before next state
+        after - int - number of notes after next state
+        mode - int in range(3) - mode of the chain(see above)
+        
+        Outputs - Markov object
+        """
+        
+        if mode == 0:
+            assert before != 0 and after == 0
+        elif mode == 1:
+            assert before == 0 and after != 0
+        else:
+            assert before != 0 and after != 0
+
+        self.before = before
+        self.after = after
+        self.mode = mode
+        self.state_dict = {}
+        
+    def add_data(self, seq, result):
+        """
+        Add one (current state -> next state) instance to the 
+        dictionary. Store each instance as a tally, to be 
+        normalized later.
+        
+        Inputs:
+        seq - if mode = 0, seq is a tuple of length before
+              if mode = 1, seq is a tuple of length after
+              if mode = 2, seq is a list of length two, 
+              with first element a tuple of length before 
+              and with second element a tuple of length after
+
+        result - int - single event
+        
+        Outputs: None
+        """
+        
+        if self.mode == 0:
+            assert isinstance(seq, tuple) and len(seq) == self.before
+        elif self.mode == 1:
+            assert isinstance(seq, tuple) and len(seq) == self.after
+        else:
+            assert (isinstance(seq, tuple) and 
+                    len(seq) == 2 and
+                    isinstance(seq[0], tuple) and 
+                    len(seq[0]) == self.before and
+                    isinstance(seq[1], tuple) and 
+                    len(seq[1]) == self.after)
+            
+        if seq not in self.state_dict:
+            self.state_dict[seq] = {result: 1}
+        elif result in self.state_dict[seq]:
+            self.state_dict[seq][result] += 1
+        else:
+            self.state_dict[seq][result] = 1
+            
+    def normalize(self):
+        """
+        Convert the state_dict dictionary from counts to 
+        probabilities.
+        
+        Inputs: None
+        
+        Outputs: None
+        """
+        
+        for seq in self.state_dict:
+            sum = 0
+            for result in self.state_dict[seq]:
+                sum += self.state_dict[seq][result]
+            for result in self.state_dict[seq]:
+                self.state_dict[seq][result] /= float(sum)
