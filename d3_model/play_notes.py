6aa6e82005db1a177f22cd396fba3b2a811f6b06
John Gilling <suddensleep@gmail.com>
2015-12-09 14:27:36 -0500

    readied d3 model for cloud hosting

--- d3_model/play_notes.py
+++ d3_model/play_notes.py
@@ -1,9 +1,16 @@
 import fluidsynth
 import flask
 import time
+import numpy
+import wave
+import os
+import uuid
 from collections import defaultdict
 
+
 app = flask.Flask(__name__)
+
+absolute_path = '/home/john_gilling/eb_flask_app/'
 
 def make_dict(notes):
     note_dict = defaultdict(list)
@@ -19,43 +26,54 @@ def make_dict(notes):
 
     return note_dict
 
+
 @app.route('/')
 def home_page():
-    with open('piano.html', 'r') as f:
+    with open(absolute_path + 'piano.html', 'r') as f:
         return f.read()
+
 
 @app.route('/play', methods = ['POST'])
 def play_notes():
 
     data = flask.request.json
 
+    rand_id = str(uuid.uuid4().hex)[:6]
+
     if data['notes'] == '':
         return flask.jsonify(data)
 
-    print data
-    
     note_data = make_dict(data)
 
-    print note_data
+    s = []
 
     fs = fluidsynth.Synth()
-    fs.start()
-    sfid = fs.sfload("../FluidR3_GM.sf2")
+    sfid = fs.sfload(absolute_path + "FluidR3_GM.sf2")
     fs.program_select(0, sfid, 0, 0)
 
     for i in range(16):
         if i in note_data:
             for val in note_data[i]: 
                 fs.noteon(0, int(val), 100)
-            time.sleep(.2)
+            s = numpy.append(s, fs.get_samples(int(44100 * .2))) 
             for val in note_data[i]:
                 fs.noteoff(0, int(val))
         else:
-            time.sleep(.2)
+            s = numpy.append(s, fs.get_samples(int(44100 * .2)))
 
     fs.delete()
 
-    return flask.jsonify(data)
+    samps = fluidsynth.raw_audio_string(s)
+
+    wf = wave.open(absolute_path + 'static/' + rand_id + '.wav', 'wb')
+    wf.setnchannels(2)
+    wf.setframerate(44100)
+    wf.setsampwidth(2)
+    wf.writeframes(b''.join(samps))
+    wf.close()
+
+    return flask.jsonify({'id': rand_id})
+
 
 @app.route('/augment', methods = ['POST'])
 def augment():
@@ -72,4 +90,4 @@ def augment():
     return flask.jsonify(notes)
 
 
-app.run(debug = True)
+app.run(host='0.0.0.0', port=5000)
